8c5b8e4d1de055e298eafcc5f06f918390e1e599
chengyh <chengyh2golang@gmail.com>
2019-12-11 16:20:31 +0800

    operator-sdk new

---
+++ pkg/apis/crd/group.go
@@ -0,0 +1,6 @@
+// Package crd contains crd API versions.
+//
+// This file ensures Go source parsers acknowledge the crd package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package crd
