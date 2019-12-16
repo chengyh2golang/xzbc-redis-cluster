d5b4ef2ab3bdeb2d7f9de73ef4aaeac323600f00
chengyh2golang <chengyh2golang@gmail.com>
2019-12-16 12:49:02 +0800

    test

--- pkg/controller/rediscluster/rediscluster_controller.go
+++ pkg/controller/rediscluster/rediscluster_controller.go
@@ -14,7 +14,6 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
